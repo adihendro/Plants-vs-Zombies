390d04a9513a9a32d06f207c52a47e369fdeeafa
adrielsitumorang <54342857+adrielsitumorang@users.noreply.github.com>
2020-04-15 21:55:08 +0700

    Update World.java

--- src/World.java
+++ src/World.java
@@ -57,7 +57,7 @@ public class World extends JPanel implements ActionListener{
         timer = new Timer(25, this); //set up timer for 25 milliseconds
 
         try{ //load image
-            img[0]=Toolkit.getDefaultToolkit().getImage(getClass().getResource("Assets/image/Menu.jpg"));
+            img[0]=Toolkit.getDefaultToolkit().getImage(getClass().getResource("Assets/image/Menu.jpg")); // Untuk load menu awal
         }catch(Exception ex){
             ex.printStackTrace();
             JOptionPane.showMessageDialog(null, "Cannot open image!"); //show error dialog
