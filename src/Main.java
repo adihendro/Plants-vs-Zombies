8626a9626ee066eb1b71dbc9901d7d242eca5763
adihendro <adi21hendro@gmail.com>
2020-04-15 22:18:09 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main{
         
         frame.add(new World());
         frame.setTitle("Plants vs Zombies");
-        frame.setBounds(130,0, 1024, 625+screenInsets.top);
+        frame.setBounds(130,0, 1024, 655+screenInsets.top);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
